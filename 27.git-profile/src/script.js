c5c960e984bd3d024aced474598ed7701f6480af
MD TAZRI <mdtazri@gmail.com>
2021-06-19 15:47:17 +0600

    complate github profile site mini project

---
+++ 27.git-profile/src/script.js
@@ -0,0 +1,111 @@
+// track element
+let form = document.querySelector('form');
+let input = document.querySelector('input');
+let main = document.querySelector('main');
+
+// url
+const userURL = 'https://api.github.com/users/';
+
+// add event listener on form
+form.addEventListener('submit',event=>{
+    event.preventDefault();
+
+    const user =  input.value.trim();
+    
+    getUser(user);
+})
+
+// getUser function
+async function getUser(userName){
+    // try to find out user
+     try{
+        const {data} = await axios(userURL+userName);
+        
+        main.innerHTML = '';
+        createUser(data);
+        addRepos(userName);
+     }catch(error){
+        main.innerHTML = '';
+         userNotFound();
+     }
+}
+
+// createUser function
+function createUser(data){
+    let {
+        name,
+        avatar_url : avatar,
+        public_repos : repos,
+        followers,
+        following,
+        location,
+        bio
+    } = data;
+
+    let nameNotFound = 'anonymous';
+    let locationNotFound = 'Location is not found';
+    let bioNotFound = 'This person can not write his bio.Probably This person is not active in github or delete the account for some reason.';
+
+    
+    // const create div and push the contain about user
+    const div = document.createElement('div');
+    div.className = 'profile';
+
+    div.innerHTML = `
+    <!-- start div profile -->
+            <!-- start img -->
+            <div class="profile-avater">
+                <img src="${avatar}" alt="${name}">
+            </div>
+
+            <!-- profile info -->
+            <div class="profile-info">
+                <h2>${name ? name : nameNotFound}</h2>
+                <p>${location ? location : locationNotFound}</p>
+
+                <p class="bio">${bio ? bio : bioNotFound}</p>
+
+                <ul class="status">
+                    <li>${followers} followers</li>
+                    <li>${following} following</li>
+                    <li>${repos} repos</li>
+                </ul>
+
+                <div class="repos">
+                </div>
+            </div>
+            <!-- profile info end -->
+        <!-- end div profile --> 
+    `
+    main.appendChild(div);
+}
+
+// userNotFound
+function userNotFound(){
+    let h1 = document.createElement('h1');
+    h1.className = 'not-found';
+    h1.innerText = '404 NOT FOUND';
+
+    main.appendChild(h1);
+}
+
+
+// addRepos
+async function addRepos(userName){
+    let {data} = await axios(userURL+userName+'/repos?sort=created');
+
+    data.slice(0,10).forEach(({html_url,name})=>{
+        let span = document.createElement('span');
+        let a = document.createElement('a');
+
+        a.innerText = name;
+        a.href = html_url;
+        a.target = '_blank';
+        span.appendChild(a);
+
+        document.querySelector('.repos').appendChild(span);
+        console.log();
+    })
+}
+
+//  <h1 class="not-found">404 NOT FOUND</h1> 
